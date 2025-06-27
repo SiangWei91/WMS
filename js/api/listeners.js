524ec2c8b65069f0557705e5d84c7e3abe151cb7
SiangWei91 <siangwei91@gmail.com>
2025-06-27 11:28:37 +0800

    Create listeners.js

---
+++ js/api/listeners.js
@@ -0,0 +1,8 @@
+// Firestore listener unsubscribe variables
+let productsListenerUnsubscribe = null;
+let inventoryListenerUnsubscribe = null;
+let transactionListenerUnsubscribe = null;
+let shipmentListenerUnsubscribe = null;
+
+// It might be beneficial to group these or manage them within their respective API modules in the future.
+// For now, this replicates the existing global variables.
